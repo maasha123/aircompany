475f80df273c7da455b0d10d5d0659bd93db14d1
Mari <70571889+maasha123@users.noreply.github.com>
2021-11-07 18:15:38 +0300

    Update MyPlane.java

--- Java/src/main/java/Planes/MyPlane.java
+++ Java/src/main/java/Planes/MyPlane.java
@@ -1,61 +1,9 @@
 package Planes;
 
-import java.util.Objects;
-
-abstract public class Plane {
-
-    String modelOfPlane;
-    private int maxSpeed;
-    private int maxFlightDistance;
-    private int maxLoadCapacity;
-     
-    public Plane(String modelOfPlane, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
-        this.modelOfPlane = modelOfPlane;
-        this.maxSpeed = maxSpeed;
-        this.maxFlightDistance = maxFlightDistance;
-        this.maxLoadCapacity = maxLoadCapacity;
+public class MyPlane  extends Plane {
+    public MyPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
     }
 
-    public String getPlaneModel() {
-        return modelOfPlane;
-    }
-
-    public int getMaxPlaneSpeed() {
-        return maxSpeed;
-    }
-
-    public int GetMaxFlightDistance() {
-        return maxFlightDistance;
-    }
-
-    public int getMaxLoadCapacity() {
-        return maxLoadCapacity;
-    }
-
-    @Override
-    public boolean equals(Object plane) {
-        if (this == plane) return true;
-        if (!(plane instanceof Plane)) return false;
-        Plane currentPlane = (Plane) plane;
-        return maxSpeed == currentPlane.maxSpeed &&
-                maxFlightDistance == currentPlane.maxFlightDistance &&
-                maxLoadCapacity == currentPlane.maxLoadCapacity &&
-                Objects.equals(modelOfPlane,currentPlane.modelOfPlane);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(modelOfPlane, maxSpeed, maxFlightDistance, maxLoadCapacity);
-    }
-
-    @Override
-    public String toString() {
-        return "Plane{" +
-                "modelOfPlane='" + modelOfPlane + '\'' +
-                ", maxSpeed=" + maxSpeed +
-                ", maxFlightDistance=" + maxFlightDistance +
-                ", maxLoadCapacity=" + maxLoadCapacity +
-                '}';
-    }
+    //TODO implement it later
 }
-
