1b7ecdd65f7f9573f12c2eb5e258cac927131386
Mari <70571889+maasha123@users.noreply.github.com>
2021-11-07 18:14:53 +0300

    Update Plane.java

--- Java/src/main/java/Planes/Plane.java
+++ Java/src/main/java/Planes/Plane.java
@@ -3,58 +3,58 @@ package Planes;
 import java.util.Objects;
 
 abstract public class Plane {
-    String model;
+
+    String modelOfPlane;
     private int maxSpeed;
     private int maxFlightDistance;
     private int maxLoadCapacity;
-
-    public Plane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
-        this.model = model;
+     
+    public Plane(String modelOfPlane, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        this.modelOfPlane = modelOfPlane;
         this.maxSpeed = maxSpeed;
         this.maxFlightDistance = maxFlightDistance;
         this.maxLoadCapacity = maxLoadCapacity;
     }
 
-    public String getModel() {
-        return model;
+    public String getPlaneModel() {
+        return modelOfPlane;
     }
 
-    public int getMS() {
+    public int getMaxPlaneSpeed() {
         return maxSpeed;
     }
 
-    public int Get_Max_Flight_Distance() {
+    public int GetMaxFlightDistance() {
         return maxFlightDistance;
     }
 
-    public int getMinLoadCapacity() {
-        int result = this.maxLoadCapacity;
-        return result;
+    public int getMaxLoadCapacity() {
+        return maxLoadCapacity;
+    }
+
+    @Override
+    public boolean equals(Object plane) {
+        if (this == plane) return true;
+        if (!(plane instanceof Plane)) return false;
+        Plane currentPlane = (Plane) plane;
+        return maxSpeed == currentPlane.maxSpeed &&
+                maxFlightDistance == currentPlane.maxFlightDistance &&
+                maxLoadCapacity == currentPlane.maxLoadCapacity &&
+                Objects.equals(modelOfPlane,currentPlane.modelOfPlane);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(modelOfPlane, maxSpeed, maxFlightDistance, maxLoadCapacity);
     }
 
     @Override
     public String toString() {
         return "Plane{" +
-                "model='" + model + '\'' +
+                "modelOfPlane='" + modelOfPlane + '\'' +
                 ", maxSpeed=" + maxSpeed +
                 ", maxFlightDistance=" + maxFlightDistance +
                 ", maxLoadCapacity=" + maxLoadCapacity +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Plane)) return false;
-        Plane plane = (Plane) o;
-        return maxSpeed == plane.maxSpeed &&
-                maxFlightDistance == plane.maxFlightDistance &&
-                maxLoadCapacity == plane.maxLoadCapacity &&
-                Objects.equals(model, plane.model);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
     }
 }
