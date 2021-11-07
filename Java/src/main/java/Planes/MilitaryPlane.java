3ffd27a610929ec740ef6d57239392717dcf6df7
Mari <70571889+maasha123@users.noreply.github.com>
2021-11-07 18:12:42 +0300

    MilitaryPlane.java

--- Java/src/main/java/Planes/MilitaryPlane.java
+++ Java/src/main/java/Planes/MilitaryPlane.java
@@ -6,35 +6,35 @@ import java.util.Objects;
 
 public class MilitaryPlane extends Plane{
 
-    private MilitaryType type;
+    private MilitaryType militaryType;
 
-    public MilitaryPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType type) {
-        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-        this.type = type;
+    public MilitaryPlane(String modelOfPlane, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType type) {
+        super(modelOfPlane, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.militaryType = type;
     }
 
-    public MilitaryType getType() {
-        return type;
+    public MilitaryType getMilitaryType() {
+        return militaryType;
+    }
+
+    @Override
+    public boolean equals(Object plane) {
+        if (this == plane) return true;
+        if (!(plane instanceof MilitaryPlane)) return false;
+        if (!super.equals(plane)) return false;
+        MilitaryPlane currentPlane = (MilitaryPlane) plane;
+        return militaryType == currentPlane.militaryType;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), militaryType);
     }
 
     @Override
     public String toString() {
         return super.toString().replace("}",
-                ", type=" + type +
+                ", type=" + militaryType +
                 '}');
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof MilitaryPlane)) return false;
-        if (!super.equals(o)) return false;
-        MilitaryPlane that = (MilitaryPlane) o;
-        return type == that.type;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), type);
     }
 }
