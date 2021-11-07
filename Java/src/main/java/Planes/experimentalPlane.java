e260d8e780f5cfc29becf9553154e4280a9c131e
Mari <70571889+maasha123@users.noreply.github.com>
2021-11-07 18:11:59 +0300

    ExperimentalPlane.java

--- Java/src/main/java/Planes/experimentalPlane.java
+++ Java/src/main/java/Planes/experimentalPlane.java
@@ -1,41 +1,58 @@
 package Planes;
 
+import java.util.Objects;
+
 import models.ClassificationLevel;
 import models.ExperimentalTypes;
 
-public class experimentalPlane extends Plane{
+public class ExperimentalPlane extends Plane{
 
-    private ExperimentalTypes type;
+    private ExperimentalTypes experimentalType;
     private ClassificationLevel classificationLevel;
-
-    public experimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
-        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-        this.type = type;
+    
+    public ExperimentalPlane(String modelOfPlane, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, 
+                             ClassificationLevel classificationLevel) {
+        super(modelOfPlane, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.experimentalType = type;
         this.classificationLevel = classificationLevel;
     }
 
-    public ClassificationLevel getClassificationLevel(){
-        return classificationLevel;
+    public ExperimentalTypes getExperimentalType() {
+        return experimentalType;
     }
 
-    public void setClassificationLevel(ClassificationLevel classificationLevel){
+    public ClassificationLevel getClassificationLevel() {
+        return classificationLevel;
+    }
+    
+    public void setExperimentalType(ExperimentalTypes experimentalType) {
+        this.experimentalType = experimentalType;
+    }
+
+    public void setClassificationLevel(ClassificationLevel classificationLevel) {
         this.classificationLevel = classificationLevel;
     }
 
     @Override
-    public boolean equals(Object o) {
-        return super.equals(o);
+    public boolean equals(Object plane) {
+        if (this == plane) return true;
+        if (!(plane instanceof ExperimentalPlane)) return false;
+        if (!super.equals(plane)) return false;
+        ExperimentalPlane currentPlane = (ExperimentalPlane) plane;
+        return experimentalType == currentPlane.experimentalType
+               && classificationLevel == currentPlane.classificationLevel;
     }
 
     @Override
     public int hashCode() {
-        return super.hashCode();
+        return Objects.hash(super.hashCode(), experimentalType, classificationLevel);
     }
 
     @Override
     public String toString() {
-        return "experimentalPlane{" +
-                "model='" + model + '\'' +
-                '}';
+        return super.toString().replace("}",
+                ", experimentalType=" + experimentalType +
+                        ", classificationLevel=" + classificationLevel +
+                        '}');
     }
 }
